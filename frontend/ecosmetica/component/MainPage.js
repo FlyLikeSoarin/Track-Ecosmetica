78b3d3b355f0075a8769975a6a24de03aaa17643
Maria <maria150@mail.ru>
2020-10-03 18:36:32 +0300

    home screen

--- frontend/ecosmetica/component/MainPage.js
+++ frontend/ecosmetica/component/MainPage.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, ImageBackground } from 'react-native';
+import barchartImage from '../static/GKQKMxrWAK0.jpg';
+import im from '../static/B12Ha_j6nzc.jpg';
 
 
 export default class MainPage extends React.Component {
 
+ 
   render() {
-
+    //const image = { uri: '../static/B12Ha_j6nzc.jpg'};
+    //const image = { uri: "https://reactjs.org/logo-og.png" };
     return (
       <View
         style={{
@@ -13,11 +17,30 @@ export default class MainPage extends React.Component {
           flexDirection: 'column',
           justifyContent: 'flex-end',
         }}>
-
-        <Button
+        <View
+          style={{
+              flex: 5,
+          }}
+        >
+          <ImageBackground source={im}/>
+        </View>
+        <View
+          style={{
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+              flexGrow: 1,
+          }}>
+        
+          <Button
             title={'Scan'}
             onPress={this.props.openScan}
-          />
+            color={'red'}
+          >
+            <ImageBackground source={barchartImage}></ImageBackground>
+          </Button>
+          <img width={80} height={80} src = {barchartImage}/>
+        </View>
       </View>
     );
   }
---
+++ frontend/ecosmetica/component/ResultPage.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+
+
+export default class ResultPage extends React.Component {
+
+  render() {
+
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+          backgroundImage: 'url("./static/B12Ha_j6nzc.jpg")',
+        }}>
+
+      </View>
+    );
+  }
+}
