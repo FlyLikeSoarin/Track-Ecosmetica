481828bc1fe07b0c778921f1eab4143957aae356
Maria <maria150@mail.ru>
2020-10-06 17:05:50 +0300

    product page

--- frontend/ecosmetica/component/MainPage.js
+++ frontend/ecosmetica/component/MainPage.js
@@ -1,46 +1,80 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Button, ImageBackground } from 'react-native';
-import barchartImage from '../static/GKQKMxrWAK0.jpg';
-import im from '../static/B12Ha_j6nzc.jpg';
+import { Text, View, StyleSheet, Button, ImageBackground, TouchableOpacity, Image } from 'react-native';
+import barchartImage from '../static/plus-positive-add-mathematical-symbol.svg';
+import backgroundImage from '../static/bottles-mock.jpg';
 
+const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    backgroundColor: '#9ae7af',
+  },
+  headerText: {
+    color: '#ffffff',
+    fontSize: 40,
+    fontFamily: 'Forum',
+    textAlign: 'center',
+  },
+  productImage: {
+    flex: 5,
+  },
+  containerProductText: {
+    padding: 20,
+  },
+  productText: {
+    color: '#467354',
+    fontSize: 40,
+    fontFamily: 'Forum',
+    textAlign: 'center',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'flex-center',
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  },
+  buttonMenuContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexGrow: 1,
+    backgroundColor: '#9ae7af',
+  },
+  buttonImage: {
+    color:'#9ae7af',
+  },
+  img: {
+    width: 80,
+    height: 80,
+  },
+})
 
 export default class MainPage extends React.Component {
-
  
   render() {
-    //const image = { uri: '../static/B12Ha_j6nzc.jpg'};
-    //const image = { uri: "https://reactjs.org/logo-og.png" };
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'flex-end',
-        }}>
-        <View
-          style={{
-              flex: 5,
-          }}
-        >
-          <ImageBackground source={im}/>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Продукт</Text>
         </View>
-        <View
-          style={{
-              flex: 1,
-              flexDirection: 'row',
-              justifyContent: 'space-around',
-              flexGrow: 1,
-          }}>
-        
-          <Button
-            title={'Scan'}
-            onPress={this.props.openScan}
-            color={'red'}
-          >
-            <ImageBackground source={barchartImage}></ImageBackground>
-          </Button>
-          <img width={80} height={80} src = {barchartImage}/>
+        <View style={styles.productImage}>
+          <ImageBackground source={backgroundImage} style={styles.image}>
+          <View></View>
+          </ImageBackground>
         </View>
+        <View style={styles.containerProductText}>
+          <Text style={styles.productText}>У вас пока нет отсканированных продуктов</Text>
+        </View>
+        <View style={styles.buttonMenuContainer}>
+            <TouchableOpacity
+              title={'Scan'}
+              onPress={this.props.openScan}
+            >
+              <img width={70} heigth={70} src={barchartImage}/>
+            </TouchableOpacity>  
+        </View>   
       </View>
     );
   }
