f3320aebf801115505a37d4f6f3a352de414b209
Vihrev Evgenij <evgeny.vihrev@gmail.com>
2020-10-18 17:34:32 +0300

    Allow cors headers

--- backend/ecosmetica/settings.py
+++ backend/ecosmetica/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8005",
+    "192.168.0.105",
+]
+
 AUTH_USER_MODEL = 'Users.User'
 
 
@@ -39,12 +44,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheareds',
     'rest_framework',
     'Cosmetics',
     'Users',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
