5718defc5c7a4f3cfd33601f3c64215460b4d36d
root <root@kvmru05-19801.fornex.org>
2020-10-20 18:28:05 +0200

    fixes to URL

--- backend/Cosmetics/urls.py
+++ backend/Cosmetics/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from Cosmetics import views
 
 urlpatterns = [
-    path('product/history',  views.ProductHistoryView.as_view()),
-    path('product/', views.ProductRetrieveCreateView.as_view()),
+    path('history/',  views.ProductHistoryView.as_view()),
+    path('', views.ProductRetrieveCreateView.as_view()),
 ]
