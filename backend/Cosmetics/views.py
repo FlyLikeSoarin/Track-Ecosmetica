cab69e59b345f16f9607b00bd14b219f936d6d67
Vihrev Evgenij <evgeny.vihrev@gmail.com>
2020-10-24 19:42:49 +0300

    fix

--- backend/Cosmetics/views.py
+++ backend/Cosmetics/views.py
@@ -152,7 +152,7 @@ class AnalyzeIngredientImageView(APIView):
         data = serializer.validated_data
         # return Response(data)
 
-        response = image_to_text(data['content']).decode('utf-8')
+        response = bytes(str(image_to_text(data['content'])), 'ascii').decode('utf-8')
         text = response[response.rfind('text:'): -1]
         words = text.split()
 
