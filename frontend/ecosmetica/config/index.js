1ea4d9387173abc164c33a6dd4fdfa8bca9a742e
tanya <tanya@localhost.localdomain>
2021-03-26 23:55:53 +0300

    fix

--- frontend/ecosmetica/config/index.js
+++ frontend/ecosmetica/config/index.js
@@ -7,5 +7,21 @@ export default {
      /**
       * VK API URL 
       */
-      
+     VK_URL: 'https://api.vk.com/method',
+
+     /**
+      * Album id for uploading photos. 
+      */
+     ALBUM_ID: '275980882',
+
+     /**
+      * Groub id for uploading photos
+      */
+     GROUP_ID:'199800692',
+
+     /**
+      * VK access token
+      */
+     ACCESS_USER_TOKEN: 'YOUR_TOKEN'
+
 }
